37fef0760beb12c5cac1d489c08ca3ad5ab3f177
ZyxpaKarymova <z_karimova@list.ru>
2025-03-14 18:13:40 +0700

    interfaces

--- src/App.java
+++ src/App.java
@@ -1,7 +1,9 @@
-import org.skypro.skyshop.basket.ProductBasket;
+import org.skypro.skyshop.article.Article;
 import org.skypro.skyshop.product.DiscountedProduct;
 import org.skypro.skyshop.product.FixPriceProduct;
 import org.skypro.skyshop.product.SimpleProduct;
+import org.skypro.skyshop.search.SearchEngine;
+import org.skypro.skyshop.search.Searchable;
 
 public class App {
     public static void main(String[] args) {
@@ -10,30 +12,35 @@ public class App {
         DiscountedProduct product2 = new DiscountedProduct("Дыня", 230, 10);
         FixPriceProduct product3 = new FixPriceProduct("Мандарин");
 
-        // Создаем корзину
-        ProductBasket basket = new ProductBasket();
+        Article article1 = new Article("Когда можно есть арбузы", "Арбузы лучше есть летом в августе");
+        Article article2 = new Article("Где растет дыня", "Дыня растет на бахче.");
 
-        // Добавляем продукты в корзину
-        basket.addProduct(product1);
-        basket.addProduct(product2);
-        basket.addProduct(product3);
+        // Создаем SearchEngine
+        SearchEngine searchEngine = new SearchEngine(10);
 
-        // Печать содержимого корзины
-        basket.printBasket();
+        // Добавляем товары и статьи в SearchEngine
+        searchEngine.add(product1);
+        searchEngine.add(product2);
+        searchEngine.add(product3);
+        searchEngine.add(article1);
+        searchEngine.add(article2);
 
-        // Получение общей стоимости корзины
-        System.out.println("Общая стоимость корзины: " + basket.getTotalCost());
-
-        // Очистка корзины
-        basket.clearBasket();
-
-        // Печать содержимого пустой корзины
-        basket.printBasket();
-
-        // Получение стоимости пустой корзины
-        System.out.println("Общая стоимость пустой корзины: " + basket.getTotalCost());
-
-        // Поиск товара по имени в пустой корзине
-        System.out.println("Есть ли в корзине Арбуз? " + basket.containsProduct("Арбуз"));
+        // Демонстрация поиска
+        System.out.println("Результаты поиска по запросу 'арбуз':");
+        Searchable[] results = searchEngine.search("Арбуз");
+        for (Searchable result : results) {
+            if (result != null) {
+                System.out.println(result.getStringRepresentation());
+            }
+        }
+        System.out.println("\nРезультаты поиска по запросу 'дыня':");
+        results = searchEngine.search("Дыня");
+        for (Searchable result : results) {
+            if (result != null) {
+                System.out.println(result.getStringRepresentation());
+            }
+        }
     }
 }
+
+
